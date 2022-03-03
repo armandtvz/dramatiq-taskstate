f5c34a1b6f2d16e9827dba57e5031ee44856037e
Armandt van Zyl <armandtvz@gmail.com>
2022-03-03 09:40:01 +0200

    Update tasks.py

--- taskstate/tasks.py
+++ taskstate/tasks.py
@@ -6,5 +6,6 @@ from taskstate.models import Task
 @dramatiq.actor(max_retries=0)
 def cleanup_tasks():
     Task.objects.delete_old(max_task_age=120)
+    Task.objects.delete_old(max_task_age=120, only_if_seen=False)
     Task.objects.delete_stale()
     return
